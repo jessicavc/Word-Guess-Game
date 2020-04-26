8ecd1646c4e38d8c91c02f5c4f76566172f7b133
Jessica VC <jessicavc@this-guy-ii.home>
2020-04-25 20:50:22 -0700

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 //computer choices for words
-var answers = ["annika","spot","picard","pike","bones","roddenberry", "futile","baseball", "doctor", "coffee"];
+var answers = ["annika", "spot", "picard", "pike", "bones", "roddenberry", "futile", "baseball", "doctor", "coffee"];
 
-var questions = ["What is Seven of Nine's human name?", "What is Data name his cat?", "What is the last name of the Captain of the Enterprise-D?", "Who was the first captain to appear in the Star Trek franchise? (Hint: Currently on Discovery)","What is Dr. Leonard McCoy's nickname?",
-"Star Trek creator: Gene _______________ ", "We are borg. Resistance is ___________. Prepare to be Assimilated.","What is Benjamin Sisko's favorite sport?", "What is the name of the EMH (Emergency Medical Hologram) on Voyager?", "Famous quote by Kathryn Janeway: 'Theres ________ in that nebula!'"];
+var questions = ["What is Seven of Nine's human name?", "What is Data name his cat?", "What is the last name of the Captain of the Enterprise-D?", "Who was the first captain to appear in the Star Trek franchise? (Hint: Currently on Discovery)", "What is Dr. Leonard McCoy's nickname?",
+    "Star Trek creator: Gene _______________ ", "We are borg. Resistance is ___________. Prepare to be Assimilated.", "What is Benjamin Sisko's favorite sport?", "What is the name of the EMH (Emergency Medical Hologram) on Voyager?", "Famous quote by Kathryn Janeway: 'Theres ________ in that nebula!'"];
 
 //global variables
 var computerGuess = "";
@@ -30,7 +30,7 @@ function wordGuess() {
     totalBlanks = letters.length;
 
     //for loop to match underscores to word length
-    for (var i=0; i < totalBlanks; i++){
+    for (var i = 0; i < totalBlanks; i++) {
         underscore.push("_");
     }
 
@@ -41,52 +41,52 @@ function wordGuess() {
     //if/else statements to give hints to users. includes pictures
 
     //annika
-    if (computerGuess === answers[0]){
+    if (computerGuess === answers[0]) {
         document.getElementById("questions").innerHTML = " " + questions[0];
         document.getElementById("image").src = "assets/images/seven.jpg";
     }
     //spot
-    else if (computerGuess === answers[1]){
+    else if (computerGuess === answers[1]) {
         document.getElementById("questions").innerHTML = " " + questions[1];
         document.getElementById("image").src = "assets/images/data-and-spot.jpg";
     }
     //picard
-    else if (computerGuess === answers[2]){
+    else if (computerGuess === answers[2]) {
         document.getElementById("questions").innerHTML = " " + questions[2];
         document.getElementById("image").src = "assets/images/picard.jpg";
     }
     //pike
-    else if (computerGuess === answers[3]){
+    else if (computerGuess === answers[3]) {
         document.getElementById("questions").innerHTML = " " + questions[3];
         document.getElementById("image").src = "assets/images/pike.png";
     }
     //bones
-    else if (computerGuess === answers[4]){
+    else if (computerGuess === answers[4]) {
         document.getElementById("questions").innerHTML = " " + questions[4];
         document.getElementById("image").src = "assets/images/bones.jpg";
     }
     //roddenberry
-    else if (computerGuess === answers[5]){
+    else if (computerGuess === answers[5]) {
         document.getElementById("questions").innerHTML = " " + questions[5];
         document.getElementById("image").src = "assets/images/roddenberry.jpg";
     }
     //futile
-    else if (computerGuess === answers[6]){
+    else if (computerGuess === answers[6]) {
         document.getElementById("questions").innerHTML = " " + questions[6];
         document.getElementById("image").src = "assets/images/resistance-is-futile.jpg";
     }
     //baseball
-    else if (computerGuess === answers[7]){
+    else if (computerGuess === answers[7]) {
         document.getElementById("questions").innerHTML = " " + questions[7];
         document.getElementById("image").src = "assets/images/sisko.jpg";
     }
     //doctor
-    else if (computerGuess === answers[8]){
+    else if (computerGuess === answers[8]) {
         document.getElementById("questions").innerHTML = " " + questions[8];
         document.getElementById("image").src = "assets/images/doctor.jpg";
     }
     //coffee
-    else if (computerGuess === answers[9]){
+    else if (computerGuess === answers[9]) {
         document.getElementById("questions").innerHTML = " " + questions[9];
         document.getElementById("image").src = "assets/images/janeway.jpg";
     }
@@ -95,30 +95,30 @@ function wordGuess() {
 //call game function
 wordGuess()
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
     checkLetters(guesses);
     end();
 }
 
 //if/else to check if letter appears in the computerGuess
-function checkLetters(letter){
+function checkLetters(letter) {
     var letterInWord = false;
 
     //win if letter appears in the array
-    for (var i =0; i<totalBlanks; i++){
-        if (computerGuess[i] == letter){
+    for (var i = 0; i < totalBlanks; i++) {
+        if (computerGuess[i] == letter) {
             letterInWord = true;
         }
     }
-  //loss logic if letter does not appear in the array
-    if (letterInWord){
-        for (var i=0; i<totalBlanks; i++){
-            if(computerGuess[i] == letter){
+    //loss logic if letter does not appear in the array
+    if (letterInWord) {
+        for (var i = 0; i < totalBlanks; i++) {
+            if (computerGuess[i] == letter) {
                 underscore[i] = letter;
             }
         }
-    }  
+    }
 
     // wrong guess get added to lettersPlayed array
     //decrease guessesRemaining.
@@ -131,32 +131,32 @@ function checkLetters(letter){
 
 //game end function
 
-function end(){
-    if (letters.toString() == underscore.toString()){
+function end() {
+    if (letters.toString() == underscore.toString()) {
         wins++;
-        alert ("You really know your Trek!");
+        alert("You really know your Trek!");
         Reset();
         document.getElementById("wins").innerHTML = wins;
-    } else if (guessesRemaining === 0){
+    } else if (guessesRemaining === 0) {
         losses++;
-        alert ("Mr. Spock says: This is highly illogical");
+        alert("Mr. Spock says: This is highly illogical");
         Reset();
         document.getElementById("losses").innerHTML = losses;
     }
 
-//game reset after each word
+    //game reset after each word
 
-function Reset() {
-    lettersPlayed = [];
-    underscore = [];
-    guessesRemaining = 10;
-    wordGuess()
-}
+    function Reset() {
+        lettersPlayed = [];
+        underscore = [];
+        guessesRemaining = 10;
+        wordGuess()
+    }
 
-console.log('Letters played', lettersPlayed);
+    console.log('Letters played', lettersPlayed);
 
 
-//adding to the DOM
+    //adding to the DOM
     document.getElementById("underscores").innerHTML = underscore.join(" ");
     document.getElementById("guessesRemaining").innerHTML = " " + guessesRemaining;
     document.getElementById("alreadyPlayed").innerHTML = " " + lettersPlayed.join(" ");
